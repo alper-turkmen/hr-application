3e7e8fc6cd0f31a00e4b5646af5d1d03adb14bc6
Alper Turkmen <alper.turkmen@telesmart.io>
2025-08-01 16:55:30 +0300

    Cors settings

--- wisehire/settings.py
+++ wisehire/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.common.CommonMiddleware", 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -106,6 +107,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
